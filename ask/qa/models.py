8c75dd143cd59c5da5c2a6e17dc6dfb7a74df958
OttoSteel <polshinski@gmail.com>
2017-09-04 02:04:52 +0300

    create models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Question(models.Model) :
+	#id = models.IntegerField(primary_key=True)
+	objects = QuestionManager()
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User, related_name='+', default=1)
+	likes = models.ManyToManyField(User, default=1)
+	
+	def get_absolute_url(self) :
+		return '/question/%d/' % self.pk
+
+class Answer(models.Model) :
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, null=False, on_delete=models.DO_NOTHING)
+        author = models.ForeignKey(User, related_name='+', default=1)
+
+class QuestionManager(models.Manager):
+	def new(self):
+        	return self.order_by('-added_at')
+	
+	def popular(self):
+        	return self.order_by('-rating')
