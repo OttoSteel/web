1cd395a207164de77485766a907771b0a8cae49d
OttoSteel <polshinski@gmail.com>
2017-09-04 03:51:41 +0300

    change settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'qa',
+		'DATABASE_HOST' : '/var/run/mysql',
+		'USER' : 'qauser',
+		'PASSWORD' : 'qapass',
     }
 }
 
