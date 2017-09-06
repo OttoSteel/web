6a047f838b85faecd8125b6d5c7730132b1c47be
OttoSteel <polshinski@gmail.com>
2017-09-06 09:36:52 +0300

    update initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -2,6 +2,7 @@
 #sudo ln -sf /home/box/web/etc/my.cnf  /etc/mysql/my.cnf
 sudo /etc/init.d/mysql start
 
-mysql -u root -e "CREATE DATABASE qa"/*!40100 DEFAULT CHARACTER SET utf8 */ ; 
-mysql -u root -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';
-				 GRANT ALL PRIVILEGES ON qa.* TO 'qauser'@'localhost';"
+#mysql -u root -e "CREATE DATABASE qa"/*!40100 DEFAULT CHARACTER SET utf8 */ ; 
+mysql -u root -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';"
+mysql -u qauser -e "CREATE DATABASE qa;"
+				 
