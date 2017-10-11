d64d46b3382181bd86e829b7219cd7bece5462a9
OttoSteel <polshinski@gmail.com>
2017-10-11 07:57:14 +0300

    update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -27,7 +27,7 @@ def index(request):
     except ValueError:
         page = 1
     paginator = Paginator(qwests, pageLimit)
-    paginator.baseurl = 'question/?page='
+    paginator.baseurl = 'question/'
     try:
         page = paginator.page(page)
     except EmptyPage:
@@ -53,7 +53,7 @@ def popular(request):
     except ValueError:
         page = 1
     paginator = Paginator(qwests, pageLimit)
-    paginator.baseurl = 'question/?page='
+    paginator.baseurl = 'question/'
     try:
         page = paginator.page(page)
     except EmptyPage:
