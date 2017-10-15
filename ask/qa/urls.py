b2378cfa91e06e4075279086af9459e66bb9f1e6
OttoSteel <polshinski@gmail.com>
2017-10-15 03:13:02 +0300

    update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
-        url(r'^login/', views.test, name='login'),
-        url(r'^signup/', views.test, name='signup'),
-        url(r'^question/(?P<quest_id>[0-9]+)/$', views.question, name='question'),
-        url(r'^ask/', views.test, name='ask'),
-        url(r'^popular/', views.popular, name='popular'),
-        url(r'^new/', views.test, name='new'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/', views.test, name='login'),
+    url(r'^signup/', views.test, name='signup'),
+    url(r'^question/(?P<quest_id>[0-9]+)/$', views.question, name='question'),
+    url(r'^ask/', 'ask', name='ask'),
+    url(r'^answer/', 'answer', name='answer'),
+    url(r'^popular/', views.popular, name='popular'),
+    url(r'^new/', views.test, name='new'),
 ]
